a60587c96cf0c75f30fd2e29eef574eb96dddc34
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-02 22:41:47 +0530

    [Arjun] Add ; Added Switched

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,24 @@
+﻿using AddressBookSystem;
+using System;
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Welcome To Addess Book Programs==========");
+            AddressBook addressBook = new AddressBook();
+            Contact newContact = addressBook.CreateContact();
+            Console.WriteLine("*****-> Details Of Person  <-*****");
+            Console.WriteLine($"First Name: {newContact.FirstName}");
+            Console.WriteLine($"Last Name: {newContact.LastName}");
+            Console.WriteLine($"City Name: {newContact.City}");
+            Console.WriteLine($"State Name: {newContact.State}");
+            Console.WriteLine($"Zipcode: {newContact.ZipCode}");
+            Console.WriteLine($"PhoneNumber: {newContact.PhoneNumber}");
+            Console.WriteLine($"Email: {newContact.Email}");
+            Console.WriteLine("==========================================================================");
+            Console.ReadLine();
+        }
+    }
+}
