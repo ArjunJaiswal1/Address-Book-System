a0827ce96d0c7490f9697d12858ee62cd727a45e
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-03 18:34:20 +0530

    [Arjun] Add ; Added Switched

--- Address/AddressBook.cs
+++ Address/AddressBook.cs
@@ -8,5 +8,27 @@ namespace Address
 {
     public class AddressBook
     {
+        List<Contact> addressList = new List<Contact>();
+        public void AddContact(Contact contact)
+        {
+            addressList.Add(contact);
+        }
+        public void Display()
+        {
+            foreach (Contact contact in addressList)
+            {
+                Console.WriteLine(contact.FirstName + " " + contact.LastName);
+                Console.WriteLine("First Name: " + contact.FirstName);
+                Console.WriteLine("Last Name: " + contact.LastName);
+                Console.WriteLine("Address : " + contact.Address);
+                Console.WriteLine("City : " + contact.City);
+                Console.WriteLine("State : " + contact.State);
+                Console.WriteLine("Postal Code : " + contact.PostalCode);
+                Console.WriteLine("Phone Number : " + contact.PhoneNumber);
+                Console.WriteLine("Email : " + contact.Email);
+
+            }
+        }
     }
 }
+    
--- Address/Program.cs
+++ Address/Program.cs
@@ -7,7 +7,7 @@ class program
         bool flag = true;
         while (flag)
         {
-            Console.WriteLine("Enter the program number to be executed : press 1-CreateContact, 2-Exit");
+            Console.WriteLine("Enter the program number to be executed : press 1-CreateContact, 2-AddContact, 3-Exit");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
@@ -24,7 +24,26 @@ class program
                     newContact.PhoneNumber = Console.ReadLine();
                     newContact.Email = Console.ReadLine();
                     break;
+               
                 case 2:
+
+                    Console.WriteLine("Enter the Contact Information to be Add in form of FirstName, LastName, Addr, City, State, Zip, Ph.No, Email.");
+                    Console.WriteLine("Enter the Contact to be Add: ");
+                    Contact contact1 = new Contact();
+                    AddressBook addressBook = new AddressBook();
+                    addressBook.AddContact(contact1);
+                    contact1.FirstName = Console.ReadLine();
+                    contact1.LastName = Console.ReadLine();
+                    contact1.Address = Console.ReadLine();
+                    contact1.City = Console.ReadLine();
+                    contact1.State = Console.ReadLine();
+                    contact1.PostalCode = Console.ReadLine();
+                    contact1.PhoneNumber = Console.ReadLine();
+                    contact1.Email = Console.ReadLine();
+                    addressBook.Display();
+                    break;
+                
+                case 3:
                     flag = false;
                     break;
             }
